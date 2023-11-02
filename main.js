d13ad45ff5aeac5a57523f67868f284a9b9b9c61
Phuociter <phuocv992@gmail.com>
2023-11-02 15:31:40 +0700

    11/2/2023

--- main.js
+++ main.js
@@ -27,43 +27,54 @@ function showSignUp() {
 function createProduct() {
     if (localStorage.getItem('product') == null){
         var productArray = [
-            {productID:100,img:'https://cdn.tgdd.vn/Products/Images/7264/199605/g-shock-ga-700-1adr-den-up-2-org.jpg',name:'Nam GA-700-1ADR',price:2114000,brand:'G-SHOCK'},
-            {productID:101,img:'https://cdn.tgdd.vn/Products/Images/7264/199609/g-shock-ga-800-1adr-den-2-3-org.jpg',name:'Nam GA-800-1ADR',price:990000,brand:'G-SHOCK'},
-            {productID:102,img:'https://cdn.tgdd.vn/Products/Images/7264/315213/g-shock-ga-2100gb-1adr-nam-2-1.jpg',name:'Nam GA-2100GB-1ADR ',price:3533000,brand:'G-SHOCK'},
-            {productID:103,img:'https://cdn.tgdd.vn/Products/Images/7264/315214/g-shock-ga-2100sb-1adr-nam-2-1.jpg',name:'Nam GA-2100SB-1ADR ',price:3316000,brand:'G-SHOCK'},
-            {productID:104,img:'https://cdn.tgdd.vn/Products/Images/7264/311127/g-shock-dw-5600ff-8-nam-1-1.jpg',name:'Forgotten Future Nam DW-5600FF-8DR',price:2764000,brand:'G-SHOCK'},
-            {productID:105,img:'https://cdn.tgdd.vn/Products/Images/7264/311126/g-shock-ga-700ff-8a-nam-1-1.jpg ',name:'Forgotten Future Nam GA-700FF-8ADR  ',price:3178000,brand:'G-SHOCK'},
-            {productID:106,img:'https://cdn.tgdd.vn/Products/Images/7264/311125/g-shock-ga-2200ff-8adr-nam-1-1.jpg',name:'Forgotten Future  Nam GA-2200FF-8ADR',price:4224000,brand:'G-SHOCK'},
-            {productID:108,img:'https://cdn.tgdd.vn/Products/Images/7264/302231/g-shock-gma-s2100ba-4adr-nu-1.jpg',name:'Nữ GMA-S2100BA-4ADR',price:2902000,brand:'G-SHOCK'},
-            {productID:109,img:'https://cdn.tgdd.vn/Products/Images/7264/289339/g-shock-ga-2100-1a1dr-nam-1-2.jpg',name:'Nam GA-2100-1A1DR',price:2902000,brand:'G-SHOCK'},
-            {productID:110,img:'https://cdn.tgdd.vn/Products/Images/7264/283575/g-shock-gw-a1100r-4adr-nam-2-1.jpg',name:'Nam GW-A1100R-4ADR ',price:16680000,brand:'G-SHOCK'},
-            {productID:111,img:'https://content.rolex.com/v7/dam/2023-06/upright-c/m126506-0001.png?impolicy=v7&imwidth=1600',name:'Cosmograph Daytona',price:1937733000,brand:'ROLEX'},
-            {productID:112,img:'https://content.rolex.com/v7/dam/2023-06/upright-c/m279384rbr-0004.png?impolicy=v7&imwidth=1600',name:'Lady-Datejust',price:407394000,brand:'ROLEX'},
-            {productID:113,img:'https://www.vacheron-constantin.com/dam/rcq/vac/16/19/63/0/1619630.png.transform.vacprodcard.png',name:'RETROGRADE DAY-DATE',price:1280000000,brand:'VACHERON'},
-            {productID:114,img:'https://www.vacheron-constantin.com/dam/rcq/vac/16/23/85/0/1623850.png.transform.vacprodcard.png',name:'DUAL TIME',price:1260000000,brand:'VACHERON'},
-            {productID:115,img:'https://www.vacheron-constantin.com/dam/rcq/vac/16/15/83/5/1615835.png.transform.vacprodcard.png',name:'TRIPLE CALENDRIER 1942',price:625000000,brand:'VACHERON'},
-            {productID:116,img:'https://www.vacheron-constantin.com/dam/rcq/vac/19/64/96/1/1964961.png.transform.vacprodcard.png',name:'QUARTZ',price:850000000,brand:'VACHERON'},
-            {productID:117,img:'https://content.rolex.com/v7/dam/2023-06/upright-c/m126713grnr-0001.png?impolicy=v7&imwidth=1600',name:'GMT-Master II',price:410005500,brand:'ROLEX'},
-            {productID:118,img:'https://content.rolex.com/v7/dam/2023-06/upright-c/m336239-0002.png?impolicy=v7&imwidth=1600',name:'Sky-Dweller',price:1062880500,brand:'ROLEX'},
-            {productID:119,img:'https://content.rolex.com/v7/dam/2023-06/upright-c/m228236-0012.png?impolicy=v7&imwidth=1600',name:'Day-Date 40',price:1577346000,brand:'ROLEX'},
-            {productID:120,img:'https://content.rolex.com/v7/dam/2023-06/upright-c/m126234-0051.png?impolicy=v7&imwidth=1600',name:'Datejust 36',price:221977500,brand:'ROLEX'},
-            {productID:121,img:'https://content.rolex.com/v7/dam/2023-06/upright-c/m126900-0001.png?impolicy=v7&imwidth=1600',name:'Air-King',price:185416500,brand:'ROLEX'},
-            {productID:122,img:'https://content.rolex.com/v7/dam/2023-06/upright-c/m136660-0003.png?impolicy=v7&imwidth=1600',name:'Rolex Deapsea',price:360387000,brand:'ROLEX'},
-            {productID:123,img:'https://www.vacheron-constantin.com/dam/rcq/vac/20/52/87/1/2052871.png.transform.vacprodcard.png',name:'OVERSEAS CHRONOGRAPH',price:925000000,brand:'VACHERON'},
-            {productID:124,img:'https://content.rolex.com/v7/dam/2023-06/upright-c/m128348rbr-0049.png?impolicy=v7&imwidth=1600',name:'Day-Date 36',price:1462400000,brand:'ROLEX'},
-            {productID:125,img:'https://content.rolex.com/v7/dam/2023-06/upright-c/m277200-0010.png?impolicy=v7&imwidth=1600',name:'Oyster Perpetual 31',price:141021000,brand:'ROLEX'},
-            {productID:126,img:'https://content.rolex.com/v7/dam/2023-06/upright-c/m278343rbr-0016.png?impolicy=v7&imwidth=1600',name:'Datejust 31',price:396498000,brand:'ROLEX'},
-            {productID:127,img:'https://www.vacheron-constantin.com/dam/rcq/vac/S2/r-/SF/ZI/0U/CZ/PZ/CY/V3/0M/pA/S2r-SFZI0UCZPZCYV30MpA.png.transform.vacprodcard.png',name:'RETROGRADE DAY-DATE',price:1600000000,brand:'VACHERON'},
-            {productID:128,img:'https://cdn.tgdd.vn/Products/Images/7264/305813/g-shock-gm-5600-1dr-nam-1-1.jpg',name:'Nam GM-5600-1DR',price:4352000,brand:'G-SHOCK'},
-            {productID:129,img:'https://cdn.tgdd.vn/Products/Images/7264/286412/g-shock-gm-2100-1adr-nam-2-1.jpg',name:'GNam GM-2100-1ADR ',price:5803000,brand:'G-SHOCK'},
-            {productID:130,img:'https://content.rolex.com/v7/dam/2023-06/upright-c/m124273-0001.png?impolicy=v7&imwidth=1600',name:'Explorer 36',price:292488000,brand:'ROLEX'},
-            {productID:131,img:'https://www.vacheron-constantin.com/dam/rcq/vac/21/33/60/8/2133608.png.transform.vacprodcard.png',name:'AMERICAN 1921',price:865000000,brand:'VACHERON'},
-            {productID:132,img:'https://www.vacheron-constantin.com/dam/rcq/vac/20/39/25/5/2039255.png.transform.vacprodcard.png',name:'OVERSEAS SELF-WINDING',price:1570000000,brand:'VACHERON'},
-            {productID:133,img:'https://www.vacheron-constantin.com/dam/rcq/vac/16/51/57/6/1651576.png.transform.vacprodcard.png',name:'SELF-WINDING',price:329000000,brand:'VACHERON'},
-            {productID:134,img:'https://www.vacheron-constantin.com/dam/rcq/vac/16/21/86/1/1621861.png.transform.vacprodcard.png',name:'OVERSEAS SELF-WINDING',price:1140000000,brand:'VACHERON'},
-            {productID:135,img:'https://www.vacheron-constantin.com/dam/rcq/vac/r3/w5/I7/7P/IE/a0/Ki/Dw/B-/Hh/6Q/r3w5I77PIEa0KiDwB-Hh6Q.png.transform.vacprodcard.png',name:'MOON PHASE',price:2120000000,brand:'VACHERON'},
-            {productID:136,img:'https://www.vacheron-constantin.com/dam/rcq/vac/23/72/46/4/2372464.png.transform.vacprodcard.png',name:'DUAL TIME',price:800000000,brand:'VACHERON'},
-
+            {productID:100,img:  'IMG/101.jpg',name:'Nam GA-700-1ADR',price:2114000,brand:'G-SHOCK'},
+            {productID:101,img:  'IMG/102.jpg',name:'Nam GA-800-1ADR',price:990000,brand:'G-SHOCK'},
+            {productID:102,img:  'IMG/103.jpg',name:'Nam GA-2100GB-1ADR ',price:3533000,brand:'G-SHOCK'},
+            {productID:103,img:  'IMG/104.jpg',name:'Nam GA-2100SB-1ADR ',price:3316000,brand:'G-SHOCK'},
+            {productID:104,img:  'IMG/105.jpg',name:'Forgotten Future Nam DW-5600FF-8DR',price:2764000,brand:'G-SHOCK'},
+            {productID:105,img:  'IMG/106.jpg',name:'Forgotten Future Nam GA-700FF-8ADR  ',price:3178000,brand:'G-SHOCK'},
+            {productID:106,img:  'IMG/107.jpg',name:'Forgotten Future  Nam GA-2200FF-8ADR',price:4224000,brand:'G-SHOCK'},
+            {productID:108,img:  'IMG/108.jpg',name:'Nữ GMA-S2100BA-4ADR',price:2902000,brand:'G-SHOCK'},
+            {productID:109,img:  'IMG/109.jpg',name:'Nam GA-2100-1A1DR',price:2902000,brand:'G-SHOCK'},
+            {productID:110,img:  'IMG/110.jpg',name:'Nam GW-A1100R-4ADR ',price:16680000,brand:'G-SHOCK'},
+            {productID:111,img:  'IMG/111.webp',name:'Cosmograph Daytona',price:1937733000,brand:'ROLEX'},
+            {productID:112,img:  'IMG/112.webp',name:'Lady-Datejust',price:407394000,brand:'ROLEX'},
+            {productID:113,img:  'IMG/113.webp',name:'RETROGRADE DAY-DATE',price:1280000000,brand:'VACHERON'},
+            {productID:114,img:  'IMG/114.webp',name:'DUAL TIME',price:1260000000,brand:'VACHERON'},
+            {productID:115,img:  'IMG/115.webp',name:'TRIPLE CALENDRIER 1942',price:625000000,brand:'VACHERON'},
+            {productID:116,img:  'IMG/116.webp',name:'QUARTZ',price:850000000,brand:'VACHERON'},
+            {productID:117,img:  'IMG/117.webp',name:'GMT-Master II',price:410005500,brand:'ROLEX'},
+            {productID:118,img:  'IMG/118.webp',name:'Sky-Dweller',price:1062880500,brand:'ROLEX'},
+            {productID:119,img:  'IMG/119.webp',name:'Day-Date 40',price:1577346000,brand:'ROLEX'},
+            {productID:120,img:  'IMG/120.webp',name:'Datejust 36',price:221977500,brand:'ROLEX'},
+            {productID:121,img:  'IMG/121.webp',name:'Air-King',price:185416500,brand:'ROLEX'},
+            {productID:122,img:  'IMG/122.webp',name:'Rolex Deapsea',price:360387000,brand:'ROLEX'},
+            {productID:123,img:  'IMG/123.webp',name:'OVERSEAS CHRONOGRAPH',price:925000000,brand:'VACHERON'},
+            {productID:124,img:  'IMG/124.webp',name:'Day-Date 36',price:1462400000,brand:'ROLEX'},
+            {productID:125,img:  'IMG/125.webp',name:'Oyster Perpetual 31',price:141021000,brand:'ROLEX'},
+            {productID:126,img:  'IMG/126.webp',name:'Datejust 31',price:396498000,brand:'ROLEX'},
+            {productID:127,img:  'IMG/127.webp',name:'RETROGRADE DAY-DATE',price:1600000000,brand:'VACHERON'},
+            {productID:128,img:  'IMG/128.jpg',name:'Nam GM-5600-1DR',price:4352000,brand:'G-SHOCK'},
+            {productID:129,img:  'IMG/129.jpg',name:'GNam GM-2100-1ADR ',price:5803000,brand:'G-SHOCK'},
+            {productID:130,img:  'IMG/130.webp',name:'Explorer 36',price:292488000,brand:'ROLEX'},
+            {productID:131,img:  'IMG/131.webp',name:'AMERICAN 1921',price:865000000,brand:'VACHERON'},
+            {productID:132,img:  'IMG/132.webp',name:'OVERSEAS SELF-WINDING',price:1570000000,brand:'VACHERON'},
+            {productID:133,img:  'IMG/133.webp',name:'SELF-WINDING',price:329000000,brand:'VACHERON'},
+            {productID:134,img:  'IMG/134.webp',name:'OVERSEAS SELF-WINDING',price:1140000000,brand:'VACHERON'},
+            {productID:135,img:  'IMG/135.webp',name:'MOON PHASE',price:2120000000,brand:'VACHERON'},
+            {productID:136,img:  'IMG/136.webp',name:'DUAL TIME',price:800000000,brand:'VACHERON'},
+            {productID:137,img:  'IMG/137.webp',name:'CO‑AXIAL MASTER CHRONOMETER',price:147200000,brand:'OMEGA'},
+            {productID:138,img:  'IMG/138.webp',name:'AQUA TERRA 150M',price:368000000,brand:'OMEGA'},
+            {productID:139,img:  'IMG/139.webp',name:'BULLHEAD ',price:2256000000,brand:'OMEGA'},
+            {productID:140,img:  'IMG/140.webp',name:'CO‑AXIAL MASTER CHRONOMETER',price:190350000,brand:'OMEGA'},
+            {productID:141,img:  'IMG/141.webp',name:'DARK SIDE OF THE MOON',price:251450000,brand:'OMEGA'},
+            {productID:142,img:  'IMG/142.webp',name:'DARK SIDE OF THE MOON',price:175075000,brand:'OMEGA'},
+            {productID:143,img:  'IMG/143.webp',name:'PLANET OCEAN 600M',price:166850000,brand:'OMEGA'},
+            {productID:144,img:  'IMG/144.webp',name:'PLOPROF 1200M',price:296100000,brand:'OMEGA'},
+            {productID:145,img:  'IMG/145.webp',name:'PRESTIGE',price:116325000,brand:'OMEGA'},
+            {productID:146,img:  'IMG/146.webp',name:'RACING ',price:150400000,brand:'OMEGA'},
+            {productID:147,img:  'IMG/148.webp',name:'TOURBILLON',price:4551950000,brand:'OMEGA'},
+           
         ]
         localStorage.setItem('product',JSON.stringify(productArray));
     }
@@ -91,7 +102,7 @@ function showProductStranger(i) {
     let contentTemp = '';
     for(let j = 0; j < pageOfProduct[i].length; j++) {
         // onclick="customAlert(\'Bạn phải đăng nhập để mua hàng\',\'warning\')" sau id="product"
-          contentTemp += '<div class="product" onclick="productDetail('+pageOfProduct[i][j].productId+')" ><img src="'+pageOfProduct[i][j].img+'" alt="" class="product_img"><p class="product_name">'+pageOfProduct[i][j].name+'</p><p class="product_price">'+currency(pageOfProduct[i][j].price)+'</p></div>';
+          contentTemp += '<div class="product" onclick="productDetail('+pageOfProduct[i][j].productID+')" ><img src="'+pageOfProduct[i][j].img+'" alt="" class="product_img"><p class="product_name">'+pageOfProduct[i][j].name+'</p><p class="product_price">'+currency(pageOfProduct[i][j].price)+'</p></div>';
     }
     content.innerHTML = contentTemp;
 }
@@ -107,20 +118,25 @@ function showProductsByBrand(brand) {
       contentTemp += '<div class="product" onclick="productDetail(' + filteredProducts[i].productID + ')"><img src="' + filteredProducts[i].img + '" alt="" class="product_img"><p class="product_name">' + filteredProducts[i].name + '</p><p class="product_price">' + currency(filteredProducts[i].price) + '</p></div>';
     }
     content.innerHTML = contentTemp;
-  }
-  
-function productDetail(productId) {
+}
+function productDetail(productID) {
     var productDetail = document.getElementById('product_detail');
+    var productInfo = document.getElementById('infomation');
     productDetail.style.display = 'block';
+    productInfo.style.display = 'block';
     let productArray = JSON.parse(localStorage.getItem('product'));
     var i;
     for (i = 0; i < productArray.length; i++) {
-          if(productId == productArray[i].productId) break;
+          if(productID == productArray[i].productID){
+            productDetail.innerHTML = '<div id="infomation"><button class="close_detail" onclick="closeProductDetail()" type="button">&times;</button><div class="left_info"><img class="img_in_info" src="'+productArray[i].img+'" alt=""></div><div class="right_info"><h2 class="product_name">'+productArray[i].name+'</h2><h4 class="product_price">Giá : '+currency(productArray[i].price)+'</h4><h4>Số lượng : </h4><button class="quantity_crease" onclick="quantitydown()">-</button><input  id="quantity" type="text" value="1"><button class="quantity_decrease" onclick="quantityup()">+</button><button id="add_cart" onclick="cartAdd('+productArray[i].productID+'),closeProductDetail()">Thêm vào giỏ hàng</button></div></div>'
+             break;
+          }
     }
-    productDetail.innerHTML = '<div id="infomation"><button class="close_detail" onclick="closeProductDetail()" type="button">&times;</button><div class="left_info"><img class="img_in_info" src="'+productArray[i].img+'" alt=""></div><div class="right_info"><h2 class="product_name">'+productArray[i].name+'</h2><h4 class="product_price">Giá : '+currency(productArray[i].price)+'</h4><h4>Số lượng : </h4><button class="quantity_crease" onclick="quantitydown()">-</button><input  id="quantity" type="text" value="1"><button class="quantity_decrease" onclick="quantityup()">+</button><button id="add_cart" onclick="cartAdd('+productArray[i].productId+'),closeProductDetail()">Thêm vào giỏ hàng</button></div></div>'
+    // productDetail.innerHTML = '<div id="infomation"><button class="close_detail" onclick="closeProductDetail()" type="button">&times;</button><div class="left_info"><img class="img_in_info" src="'+productArray[i].img+'" alt=""></div><div class="right_info"><h2 class="product_name">'+productArray[i].name+'</h2><h4 class="product_price">Giá : '+currency(productArray[i].price)+'</h4><h4>Số lượng : </h4><button class="quantity_crease" onclick="quantitydown()">-</button><input  id="quantity" type="text" value="1"><button class="quantity_decrease" onclick="quantityup()">+</button><button id="add_cart" onclick="cartAdd('+productArray[i].productID+'),closeProductDetail()">Thêm vào giỏ hàng</button></div></div>'
 }
 function closeProductDetail() {
      document.getElementById('product_detail').style.display = 'none'
+     document.getElementById('infomation').style.display = 'none';
 }
 function quantitydown() {
     if(quantity.value != 1) {
